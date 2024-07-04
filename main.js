7c5fd7a5b83c6d96342156a2e06dc52803cc99af
mughni.s.kom <mughnimuhammad015@gmail.com>
2024-07-04 22:20:24 +0700

    k

---
+++ main.js
@@ -0,0 +1,39 @@
+function cekKhodam(event) {
+    event.preventDefault();
+    let nama = $('#search').val();
+    if (nama === '') {
+        alert('Masukkan nama terlebih dahulu!');
+    } else {
+        let status = [
+            'isi',
+            'kosong',
+        ]
+        const randomStatus = Math.floor(Math.random() * status.length);
+        if (randomStatus === 0) {
+            let khodam = [
+                'Jinn',
+                'Syaitan',
+                'Malaikat',
+                'Peri',
+                'Naga',
+                'Setan',
+                'Hantu',
+                'Jin',
+                'Iblis',
+                'Kuntilanak',
+                'Pocong'
+            ];
+            const random = Math.floor(Math.random() * khodam.length);
+            $('#result').html(`${nama.toUpperCase()} - Khodam Kamu (${khodam[random]})`);
+            $('#search').val('');
+        }
+        else {
+            $('#result').html(`${nama.toUpperCase()} - KOSONG`);
+            $('#search').val('');
+        }
+    }
+}
+
+$(document).ready(function () {
+    $('#btn-search').click(cekKhodam);
+});
